a5c1088d00c3e77a1869326779c2a64011ac94f9
Susan Zhang <susanwenzhang@gmail.com>
2019-12-05 21:46:51 +0000

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
+
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -16,3 +19,22 @@ const { fetchCoordsByIP } = require('./iss');
 //   }
 //   console.log('It worked! Returned coords:', coords);
 // });
+
+// fetchISSFlyOverTimes({ latitude: '43.63830', longitude: '-79.43010' }, (error, flyoverTimes) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log('It worked! Returned flyover times:', flyoverTimes);
+// });
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  for (let eachPass of passTimes) {
+    let datetime = new Date(0);
+    datetime.setUTCSeconds(eachPass.risetime);
+    console.log(`Next pass at ${datetime} for ${eachPass.duration} seconds!`)
+  }
+});
--- iss.js
+++ iss.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-const fetchMyIP = function(callback) {
+const fetchMyIP = function (callback) {
   request("https://api.ipify.org/?format=json", (error, response, body) => {
     if (error) {
       return callback(error, null);
@@ -8,13 +8,13 @@ const fetchMyIP = function(callback) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       return callback(Error(msg), null);
     } else {
-      const ip = JSON.parse(body);
+      const {ip} = JSON.parse(body);
       return callback(null, ip);
     }
   });
 };
 
-const fetchCoordsByIP = function(ip, callback) {
+const fetchCoordsByIP = function (ip, callback) {
   request(`https://ipvigilante.com/${ip}`, (error, response, body) => {
     if (error) {
       return callback(error, null);
@@ -22,13 +22,56 @@ const fetchCoordsByIP = function(ip, callback) {
       const msg = `Status Code ${response.statusCode} when fetching coordinates. Response: ${body}`;
       return callback(Error(msg), null);
     } else {
-      const {latitude, longitude} = JSON.parse(body).data;
-      return callback(null, {latitude, longitude});
+      const { latitude, longitude } = JSON.parse(body).data;
+      return callback(null, { latitude, longitude });
     }
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+const fetchISSFlyOverTimes = function (coords, callback) {
+  let lat = coords.latitude; let long = coords.longitude;
+  request(`http://api.open-notify.org/iss-pass.json?lat=${lat}&lon=${long}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    } else if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching flyover times. Response: ${body}`;
+      return callback(Error(msg), null);
+    } else {
+      const flyoverTimes = JSON.parse(body).response;
+      return callback(null, flyoverTimes);
+    }
+  });
+};
+
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results. 
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    } 
+    fetchCoordsByIP(ip, (error, coords) => {
+      if (error) {
+        return callback(error, null);
+      }
+      fetchISSFlyOverTimes(coords, (error, flyoverTimes) => {
+        if (error) {
+          return callback(error, null)
+        }
+        callback(null, flyoverTimes);
+      })
+    })
+  })
+}
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
 
 
 
